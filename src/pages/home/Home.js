9d7e3323fc3d7f2778a61aebd684fd8fe146c902
mauricio pc <mauricio.dallagata@reingenio.com.ar>
2021-09-16 23:04:25 -0300

    sidebar component

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideBar from '../../components/side-bar/SideBar';
+import "../home/home.css"
+
+const Home = () => {
+    return (<div>
+        
+        <SideBar></SideBar>
+
+    </div>)
+}
+
+export default Home;
