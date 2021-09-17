92e125cb7177b31bf7936edbe86f668ab724240a
Jesus Carrizo <jcarrizo15@gmail.com>
2021-09-16 23:14:08 -0300

    cambios

---
+++ src/ruta/Rutas.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from "../pages/login/Login";
+import Signup from "../pages/signup/Signup";
+
+
+const Rutas = () => {
+
+  return (<Router>
+    <div>
+      {/* <div className="btn-group">
+        <Link to="/" className="btn btn-dark">
+          Inicio
+        </Link>
+        <Link to="/Login" className="btn btn-dark">
+          Login
+        </Link>
+        <Link to="/signUp" className="btn btn-dark">
+          SignUp
+        </Link>
+
+      </div> */}
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signUp">
+          <Signup />
+        </Route>
+        <Route path="/">
+
+        </Route>
+      </Switch>
+    </div>
+  </Router>)
+}
+
+export default Rutas;
