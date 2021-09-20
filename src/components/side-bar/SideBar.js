07c73e5558d765131675d7d986779bc591f2d00f
mauricio pc <mauricio.dallagata@reingenio.com.ar>
2021-09-19 22:52:03 -0300

    up

--- src/components/side-bar/SideBar.js
+++ src/components/side-bar/SideBar.js
@@ -1,57 +1,55 @@
 import React from 'react'
 import "../side-bar/sidebar.css"
 import Profile from '../../images/profile.png';
- import List from '../../images/list.png';
+import List from '../../images/list.png';
 import Home from '../../images/home.png';
 import UserImageDefault from '../../images/user_2x.png';
- 
+
 
 const SideBar = () => {
     return (
         <div className="border">
-            <div class="container">
-            <div className="row">
-                <div className="col-auto text-center">
-                    <img className="img-profile" src={UserImageDefault} alt="user" />
+            <div className="container">
+                <div className="row">
+                    <div className="col-auto text-center">
+                        <img className="img-profile" src={UserImageDefault} alt="user" />
+                    </div>
+                    <div className="col">
+                            <h4 className="mt-2"><strong>nombre de usuario</strong></h4>
+                    </div>
                 </div>
-                <div className="col">
-                    <h2 className="mt-2">
-                        <h4><strong>nombre de usuario</strong></h4>
-                    </h2>
-                </div>
-            </div>
 
-            <div className="row mt-5">
-                <div className="col">
-                    <a>
-                    <p className="text-gray">
-                            <span><img className="img" src={Profile} alt="" />
-                        </span>
-                            
-                        &nbsp;
-                        <span className="ml-1">Mi perfil</span>
-                    </p>
-                </a>
-                <hr/>
-                <a>
-                <p className="text-gray mt-4">
-                    <span><img className="img" src={List} alt="" /></span>
-                    &nbsp;
-                    <span>Mis Pacientes</span>
-                </p>
-            </a>
-            <hr />
-            <a >
-            <p className="text-gray mt-4">
-                <span><img className="img" src={Home} alt="" /></span>
-                &nbsp;
-                <span>Mis Direcciones</span>
-                        </p>
-                        <hr />
-                    </a>
+                <div className="row mt-5">
+                    <div className="col">
+                        <a>
+                            <p className="text-gray">
+                                <span><img className="img" src={Profile} alt="" />
+                                </span>
+
+                                &nbsp;
+                                <span className="ml-1 text-center">Mi perfil</span>
+                            </p>
+                        </a>
+                        <hr/>
+                        <a>
+                            <p className="text-gray mt-4">
+                                <span><img className="img" src={List} alt="" /></span>
+                                &nbsp;
+                                <span className="ml-1">Mis Pacientes</span>
+                            </p>
+                        </a>
+                        <hr/>
+                        <a >
+                            <p className="text-gray mt-4">
+                                <span><img className="img" src={Home} alt="" /></span>
+                                &nbsp;
+                                <span className="ml-1">Mis Direcciones</span>
+                            </p>
+                            <hr/>
+                        </a>
+                    </div>
+                </div>
             </div>
-                </div>
-                </div>
         </div >)
 }
 
