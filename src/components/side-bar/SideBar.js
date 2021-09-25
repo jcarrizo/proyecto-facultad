d5f1457f53118afb1c3be06e8ff858aedbd1807a
mauricio pc <mauricio.dallagata@reingenio.com.ar>
2021-09-25 16:30:52 -0300

    .

--- src/components/side-bar/SideBar.js
+++ src/components/side-bar/SideBar.js
@@ -8,172 +8,49 @@ import UserImageDefault from '../../images/user_2x.png';
 
 const SideBar = () => {
     return (
-<<<<<<< HEAD
-        <aside class="sidebar">
-            <div class="sidebar-start">
-                <div class="sidebar-head">
-                    <a href="/" class="logo-wrapper" title="Home">
-                        <span class="sr-only">Home</span>
-                        <span class="icon logo" aria-hidden="true"></span>
-                        <div class="logo-text">
-                            <span class="logo-title">Elegant</span>
-                            <span class="logo-subtitle">Dashboard</span>
-                        </div>
-                    </a>
-                    <button class="sidebar-toggle transparent-btn" title="Menu" type="button">
-                        <span class="sr-only">Toggle menu</span>
-                        <span class="icon menu-toggle" aria-hidden="true"></span>
-                    </button>
-=======
         <div className="border">
             <div className="container-sidebar">
                 <div className="row">
                     <div className="col-auto text-center">
-                        <img src={UserImageDefault} alt="user" />
+                        <img className="img-profile" src={UserImageDefault} alt="user" />
                     </div>
                     <div className="col">
                         <h4 className="mt-2"><strong>nombre de usuario</strong></h4>
                     </div>
->>>>>>> 5e054b3fcf2e732cb07ee2dc2dcfb02dbfe7b7ba
                 </div>
-                <div class="sidebar-body">
-                    <ul class="sidebar-body-menu">
-                        <li>
-                            <a class="active" href="/"><span class="icon home" aria-hidden="true"></span>Dashboard</a>
-                        </li>
-                        <li>
-                            <a class="show-cat-btn" href="##">
-                                <span class="icon document" aria-hidden="true"></span>Posts
-                                <span class="category__btn transparent-btn" title="Open list">
-                                    <span class="sr-only">Open list</span>
-                                    <span class="icon arrow-down" aria-hidden="true"></span>
+
+                <div className="row mt-5">
+                    <div className="col">
+                        <a>
+                            <p className="text-gray">
+                                <span><img className="img" src={Profile} alt="" />
                                 </span>
-                            </a>
-                            <ul class="cat-sub-menu">
-                                <li>
-                                    <a href="posts.html">All Posts</a>
-                                </li>
-                                <li>
-                                    <a href="new-post.html">Add new post</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a class="show-cat-btn" href="##">
-                                <span class="icon folder" aria-hidden="true"></span>Categories
-                                <span class="category__btn transparent-btn" title="Open list">
-                                    <span class="sr-only">Open list</span>
-                                    <span class="icon arrow-down" aria-hidden="true"></span>
-                                </span>
-                            </a>
-                            <ul class="cat-sub-menu">
-                                <li>
-                                    <a href="categories.html">All categories</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a class="show-cat-btn" href="##">
-                                <span class="icon image" aria-hidden="true"></span>Media
-                                <span class="category__btn transparent-btn" title="Open list">
-                                    <span class="sr-only">Open list</span>
-                                    <span class="icon arrow-down" aria-hidden="true"></span>
-                                </span>
-                            </a>
-                            <ul class="cat-sub-menu">
-                                <li>
-                                    <a href="media-01.html">Media-01</a>
-                                </li>
-                                <li>
-                                    <a href="media-02.html">Media-02</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a class="show-cat-btn" href="##">
-                                <span class="icon paper" aria-hidden="true"></span>Pages
-                                <span class="category__btn transparent-btn" title="Open list">
-                                    <span class="sr-only">Open list</span>
-                                    <span class="icon arrow-down" aria-hidden="true"></span>
-                                </span>
-                            </a>
-                            <ul class="cat-sub-menu">
-                                <li>
-                                    <a href="pages.html">All pages</a>
-                                </li>
-                                <li>
-                                    <a href="new-page.html">Add new page</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="comments.html">
-                                <span class="icon message" aria-hidden="true"></span>
-                                Comments
-                            </a>
-                            <span class="msg-counter">7</span>
-                        </li>
-                    </ul>
-                    <span class="system-menu__title">system</span>
-                    <ul class="sidebar-body-menu">
-                        <li>
-                            <a href="appearance.html"><span class="icon edit" aria-hidden="true"></span>Appearance</a>
-                        </li>
-                        <li>
-                            <a class="show-cat-btn" href="##">
-                                <span class="icon category" aria-hidden="true"></span>Extentions
-                                <span class="category__btn transparent-btn" title="Open list">
-                                    <span class="sr-only">Open list</span>
-                                    <span class="icon arrow-down" aria-hidden="true"></span>
-                                </span>
-                            </a>
-                            <ul class="cat-sub-menu">
-                                <li>
-                                    <a href="extention-01.html">Extentions-01</a>
-                                </li>
-                                <li>
-                                    <a href="extention-02.html">Extentions-02</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a class="show-cat-btn" href="##">
-                                <span class="icon user-3" aria-hidden="true"></span>Users
-                                <span class="category__btn transparent-btn" title="Open list">
-                                    <span class="sr-only">Open list</span>
-                                    <span class="icon arrow-down" aria-hidden="true"></span>
-                                </span>
-                            </a>
-                            <ul class="cat-sub-menu">
-                                <li>
-                                    <a href="users-01.html">Users-01</a>
-                                </li>
-                                <li>
-                                    <a href="users-02.html">Users-02</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="##"><span class="icon setting" aria-hidden="true"></span>Settings</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="sidebar-footer">
-                <a href="##" class="sidebar-user">
-                    <span class="sidebar-user-img">
-                        <picture
-                        ><source srcset="./img/avatar/avatar-illustrated-01.webp" type="image/webp" />
-                            <img src="./img/avatar/avatar-illustrated-01.png" alt="User name"
-                            /></picture>
-                    </span>
-                    <div class="sidebar-user-info">
-                        <span class="sidebar-user__title">Nafisa Sh.</span>
-                        <span class="sidebar-user__subtitle">Support manager</span>
+
+                                &nbsp;
+                                <span className="ml-1 text-center">Mi perfil</span>
+                            </p>
+                        </a>
+                        <hr />
+                        <a>
+                            <p className="text-gray mt-4">
+                                <span><img className="img" src={List} alt="" /></span>
+                                &nbsp;
+                                <span className="ml-1">Mis Pacientes</span>
+                            </p>
+                        </a>
+                        <hr />
+                        <a >
+                            <p className="text-gray mt-4">
+                                <span><img className="img" src={Home} alt="" /></span>
+                                &nbsp;
+                                <span className="ml-1">Mis Direcciones</span>
+                            </p>
+                            <hr />
+                        </a>
                     </div>
-                </a>
+                </div>
             </div>
-        </aside>)
+        </div >)
 }
 
 export default SideBar;
