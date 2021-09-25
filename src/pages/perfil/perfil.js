83ee6a39e460464ab37f7e8af23a422e13f51bf4
Jesus Carrizo <jcarrizo15@gmail.com>
2021-09-25 19:06:05 -0300

    creacion de perfil

---
+++ src/pages/perfil/perfil.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Perfil = () => {
+  return (<div>
+    <div className="row">
+      <div className="col-2">
+        <SideBar></SideBar>
+      </div>
+
+      <div className="col-10">
+
+      </div>
+    </div>
+  </div>)
+}
+
+export default Perfil;
