5e8757672e3e77cfc864fa33662e71ab3a70806f
mauricio pc <mauricio.dallagata@reingenio.com.ar>
2021-10-11 21:39:06 -0300

    .

--- src/pages/turnos/turnos.js
+++ src/pages/turnos/turnos.js
@@ -1,136 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideBar from '../../components/side-bar/SideBar';
 import "./turnos.css"
-import { Inject, Day, Week, Month, Agenda, ScheduleComponent, Resize, DragAndDrop } from "@syncfusion/ej2-react-schedule"
 
-import { L10n } from "@syncfusion/ej2-base";
+import { Calendar, dateFnsLocalizer, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-L10n.load({
-    pt: {
-        schedule: {
-            day: "Dia",
-            week: "Semana",
-            workWeek: "Semana de trabalho",
-            month: "Mês",
-            agenda: "Agenda",
-            weekAgenda: "Agenda da semana",
-            workWeekAgenda: "Agenda da Semana de Trabalho",
-            monthAgenda: "Agenda do mês",
-            today: "Hoje",
-            noEvents: "Sem eventos",
-            emptyContainer: "Não há eventos agendados para este dia.",
-            allDay: "Dia todo",
-            start: "Começar",
-            end: "Fim",
-            more: "Mais",
-            close: "Fechar",
-            cancel: "Cancelar",
-            noTitle: "(Sem título)",
-            delete: "Excluir",
-            deleteEvent: "Este evento",
-            deleteMultipleEvent: "Excluir vários eventos",
-            selectedItems: "Itens selecionados",
-            deleteSeries: "Série inteira",
-            edit: "Editar",
-            editSeries: "Série inteira",
-            editEvent: "Este evento",
-            createEvent: "Crio",
-            subject: "Sujeito",
-            addTitle: "Adicionar título",
-            moreDetails: "Mais detalhes",
-            save: "Salve ",
-            editContent: "Como você gostaria de alterar o compromisso na série?",
-            deleteContent: "Tem certeza de que deseja excluir este evento?",
-            deleteMultipleContent:
-                "Tem certeza de que deseja excluir os eventos selecionados?",
-            newEvent: "Novo evento",
-            title: "Título",
-            location: "Localização",
-            description: "Descrição",
-            timezone: "Fuso horário",
-            startTimezone: "Iniciar fuso horário",
-            endTimezone: "Fuso horário final",
-            repeat: "Repetir",
-            saveButton: "Salve ",
-            cancelButton: "Cancelar",
-            deleteButton: "Excluir",
-            recurrence: "Recorrência",
-            wrongPattern: "O padrão de recorrência não é válido.",
-            seriesChangeAlert:
-                "Deseja cancelar as alterações feitas em instâncias específicas desta série e associá-las à série inteira novamente?",
-            createError:
-                "A duração do evento deve ser menor que a frequência com que ele ocorre. Diminua a duração ou altere o padrão de recorrência no editor de eventos de recorrência.",
-            sameDayAlert:
-                "Duas ocorrências do mesmo evento não podem ocorrer no mesmo dia.",
-            editRecurrence: "Editar recorrência",
-            repeats: "Repete",
-            alert: "Alerta",
-            startEndError: "A data final selecionada ocorre antes da data de início.",
-            invalidDateError: "O valor da data inserida é inválido.",
-            blockAlert:
-                "Os eventos não podem ser agendados dentro do intervalo de tempo bloqueado.",
-            ok: "Está bem",
-            yes: "sim",
-            no: "Não",
-            occurrence: "Ocorrência",
-            series: "Series",
-            previous: "Anterior",
-            next: "Próximo",
-            timelineDay: "Dia da linha do tempo",
-            timelineWeek: "Semana da Linha do Tempo",
-            timelineWorkWeek: "Semana de trabalho da linha do tempo",
-            timelineMonth: "Mês da linha do tempo",
-            timelineYear: "Ano da Linha do Tempo",
-            editFollowingEvent: "Eventos seguintes",
-            deleteTitle: "Excluir evento",
-            editTitle: "Editar evento",
-            beginFrom: "Começar de",
-            endAt: "Termina em"
-        },
-        recurrenceeditor: {
-            none: "Nenhum",
-            daily: "Diariamente",
-            weekly: "Semanal",
-            monthly: "Por mês",
-            month: "Mês",
-            yearly: "Anual",
-            never: "Nunca",
-            until: "Até",
-            count: "Contagem",
-            first: "Primeiro",
-            second: "Segundo",
-            third: "Terceiro",
-            fourth: "Quarto",
-            last: "Último",
-            repeat: "Repetir",
-            repeatEvery: "Repita cada",
-            on: "Repetir em",
-            end: "Fim",
-            onDay: "Dia",
-            days: "Dias)",
-            weeks: "Semana (s)",
-            months: "Mês (es)",
-            years: "Anos)",
-            every: "cada",
-            summaryTimes: "tempo (s)",
-            summaryOn: "em",
-            summaryUntil: "até",
-            summaryRepeat: "Repete",
-            summaryDay: "dias)",
-            summaryWeek: "semana (s)",
-            summaryMonth: "mês (es)",
-            summaryYear: "anos)",
-            monthWeek: "Mês Semana",
-            monthPosition: "Posição do mês",
-            monthExpander: "Expansor do mês",
-            yearExpander: "Expansor do ano",
-            repeatInterval: "Intervalo de repetição"
-        },
-        calendar: {
-            today: "Hoje"
-        }
-    }
-});
+/* const locales = {
+    "en-US": require("date-fns/locale/en-US"),
+}; */
+/* const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+}); */
+
+
+require("moment/locale/es.js")
+const localizer = momentLocalizer(moment);
+
+const events = [
+    {
+        title: "Big Meeting",
+        allDay: true,
+        start: new Date(2021, 9, 0),
+        end: new Date(2021, 9, 0),
+    },
+    {
+        title: "Vacation",
+        start: new Date(2021, 9, 7),
+        end: new Date(2021, 9, 10),
+    },
+    {
+        title: "Conference",
+        start: new Date(),
+        end: new Date(),
+    },
+];
+
 
 const Turnos = () => {
     return (<div>
@@ -140,9 +55,16 @@ const Turnos = () => {
             </div>
 
             <div className="col-10 pt-5 pr-5 pb-5">
-                <ScheduleComponent>
-                    <Inject services={[Day, Week, Month, Agenda, Resize, DragAndDrop]}></Inject>
-                </ScheduleComponent>
+                <Calendar localizer={localizer} events={events}
+                    startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} messages={{
+                        next: "sig",
+                        previous: "ant",
+                        today: "Hoy",
+                        month: "Mes",
+                        week: "Semana",
+                        day: "Día",
+                    }} />
+
             </div>
         </div>
     </div>)
