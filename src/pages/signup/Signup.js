e8b38529ba06bf98c72c9a8405477901815df9d2
JesusCarrizo <jcarrizo15@gmail.com>
2021-10-24 16:39:49 -0300

    cmabios imagen signup

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react'
-import './signup.css'
-import img from '../../images/login.jpg'
+import React, { useState } from "react";
+import "./signup.css";
+import img from "../../images/login3.jpg";
 import { useForm } from "react-hook-form";
 import { db } from "../../DB/firebase";
-import { toast } from 'react-toastify'
+import { toast } from "react-toastify";
 
 const Signup = () => {
-
   const { register, handleSubmit, reset } = useForm();
   const [datos, setDatos] = useState([]);
 
@@ -20,37 +19,37 @@ const Signup = () => {
     });
   };
 
-  const onSubmit = data => {
-    GetUser()
+  const onSubmit = (data) => {
+    GetUser();
     console.log(datos);
-
 
     //To do Las contraseñas primero
 
-
-    if (data.password !== "" && data.passwordConfirm !== "" && data.password === data.passwordConfirm) {
+    if (
+      data.password !== "" &&
+      data.passwordConfirm !== "" &&
+      data.password === data.passwordConfirm
+    ) {
       const info = {
         email: data.email,
         password: data.password,
         user: data.usuario,
-        state: true
-      }
+        state: true,
+      };
       db.collection("users").doc().set(info);
-      toast('Nuevo usuario agregado', { type: 'success', autoClose: 2000 })
+      toast("Nuevo usuario agregado", { type: "success", autoClose: 2000 });
       setTimeout(() => {
-        window.location = "/"
+        window.location = "/";
       }, 1000);
-
     } else {
-      toast('Las contraseñas no son iguales', { type: 'error', autoClose: 3000 })
+      toast("Las contraseñas no son iguales", {
+        type: "error",
+        autoClose: 3000,
+      });
     }
-
-
-
   };
 
   return (
-
     <div className="d-flex align-items-center min-vh-100 py-3 py-md-0">
       <div className="container">
         <div className="card login-card">
@@ -61,41 +60,71 @@ const Signup = () => {
             <div className="col-md-7">
               <div className="card-body">
                 <div className="brand-wrapper">
-                  <a href="index.html"><img src="Images/logo.svg" alt="First Job" className="logo"></img></a>
+                  <a href="index.html">
+                    <img
+                      src="Images/logo.svg"
+                      alt="First Job"
+                      className="logo"
+                    ></img>
+                  </a>
                 </div>
                 <p className="login-card-description">Crear cuenta</p>
                 <form onSubmit={handleSubmit(onSubmit)}>
                   <div className="form-group">
-                    <input type="text" name="usuario" className="form-control"
-                      placeholder="Nombre de usuario" required {...register("usuario")}></input>
+                    <input
+                      type="text"
+                      name="usuario"
+                      className="form-control"
+                      placeholder="Nombre de usuario"
+                      required
+                      {...register("usuario")}
+                    ></input>
                   </div>
                   <div className="form-group">
-
-                    <input type="email" name="email" className="form-control"
-                      placeholder="Email" required {...register("email")}></input>
+                    <input
+                      type="email"
+                      name="email"
+                      className="form-control"
+                      placeholder="Email"
+                      required
+                      {...register("email")}
+                    ></input>
                   </div>
                   <div className="form-group mb-4">
-
-                    <input type="password" name="password" className="form-control"
-                      placeholder="Contraseña" required {...register("password")}></input>
+                    <input
+                      type="password"
+                      name="password"
+                      className="form-control"
+                      placeholder="Contraseña"
+                      required
+                      {...register("password")}
+                    ></input>
                   </div>
                   <div className="form-group mb-4">
-
-                    <input type="password" name="passwordConfirm" className="form-control"
-                      placeholder="Confirmar Contraseña" required  {...register("passwordConfirm")}></input>
+                    <input
+                      type="password"
+                      name="passwordConfirm"
+                      className="form-control"
+                      placeholder="Confirmar Contraseña"
+                      required
+                      {...register("passwordConfirm")}
+                    ></input>
                   </div>
-                  <input className="btn btn-block login-btn mb-4" type="submit"
-                    value="Registrate" onClick={() => {
+                  <input
+                    className="btn btn-block login-btn mb-4"
+                    type="submit"
+                    value="Registrate"
+                    onClick={() => {
                       setTimeout(() => {
                         reset({
                           usuario: "",
                           email: "",
                           password: "",
-                          passwordConfirm: ""
-                        })
+                          passwordConfirm: "",
+                        });
                       }, 1000);
-
-                    }} ></input>
+                    }}
+                  ></input>
                   <a href="/login">Ya tienes cuenta?</a>
                 </form>
 
@@ -108,7 +137,8 @@ const Signup = () => {
           </div>
         </div>
       </div>
-    </div>)
-}
+    </div>
+  );
+};
 
 export default Signup;
