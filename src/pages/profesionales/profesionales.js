6ce3b615667830cb73da4726b184dbd2fbc74dc7
mauricio pc <mauricio.dallagata@reingenio.com.ar>
2021-10-26 21:22:41 -0300

    visual de profesionales

--- src/pages/profesionales/profesionales.js
+++ src/pages/profesionales/profesionales.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import SideBar from "../../components/side-bar/SideBar";
 import { db } from "../../DB/firebase";
-import { useForm } from "react-hook-form";
 import "./profesionales.css";
 
 const Profesionales = () => {
@@ -24,7 +23,8 @@ const Profesionales = () => {
           <SideBar></SideBar>
         </div>
 
-        <div className="col-10 ">
+        {/*                                                             Lista de profesionales                                   */}
+        <div className="col-5">
           <div className="container mt-5">
             <h2 className="tituloPerfil">Profesionales</h2>
             <div className="row gutters-sm">
@@ -59,6 +59,80 @@ const Profesionales = () => {
                   </div>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+
+
+        {/*                                                      Seleccionable                       */}
+        <div className="col-5 mt-5 container">
+          <h2 className="tituloPerfil">Perfil</h2>
+          <div className="row gutters-sm">
+            <div className="col-md-4 mb-3">
+              <div className="card">
+                <div className="card-body">
+                  <div className="d-flex flex-column align-items-center text-center">
+                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150"></img>
+                    <div className="mt-3 ">
+                      <h4>{"datoss.nombre" + " " + "datoss.apellido"}</h4>
+                      <p className="text-secondary mb-1">Profesión</p>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            <div className="col-md-8">
+              <div className="card mb-3">
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <h6 className="mb-0">Nombre</h6>
+                    </div>
+                    <div className="col-sm-9 text-secondary">
+                      {"datoss.nombre"}
+                    </div>
+                  </div>
+                  <hr className="mb-4"></hr>
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <h6 className="mb-0">Apellido</h6>
+                    </div>
+                    <div className="col-sm-9 text-secondary">
+                      {"datoss.apellido"}
+                    </div>
+                  </div>
+                  <hr className="mb-4"></hr>
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <h6 className="mb-0">Email</h6>
+                    </div>
+                    <div className="col-sm-9 text-secondary">
+                      {"datoss.email"}
+                    </div>
+                  </div>
+                  <hr className="mb-4"></hr>
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <h6 className="mb-0">Teléfono</h6>
+                    </div>
+                    <div className="col-sm-9 text-secondary">
+                      {"datoss.telefono"}
+                    </div>
+                  </div>
+                  <hr className="mb-4"></hr>
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <h6 className="mb-0">Domicilio</h6>
+                    </div>
+                    <div className="col-sm-9 text-secondary">
+                      {"datoss.direccion"}
+                    </div>
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
