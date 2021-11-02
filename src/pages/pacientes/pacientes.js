b9698679fe2ce9e1fb4bb510fcf109b236675d3f
mauricio pc <mauricio.dallagata@reingenio.com.ar>
2021-11-01 22:44:33 -0300

    .

--- src/pages/pacientes/pacientes.js
+++ src/pages/pacientes/pacientes.js
@@ -141,7 +141,7 @@ const Pacientes = () => {
                         </thead>
                         <tbody>
                           {datos.map((datoss) => {
-                            if (rolUsuario === "3" || rolUsuario === "2") {
+                            if ((rolUsuario === "3" || rolUsuario === "2") && datoss.eliminado === false) {
                               return (
                                 <tr onClick={() => pacienteInfo(datoss)}>
                                   <td>
@@ -154,7 +154,7 @@ const Pacientes = () => {
                                 </tr>
                               );
                             }
-                            if (datoss.profesionalId === IdUsuario) {
+                            if ((datoss.profesionalId === IdUsuario) && datoss.eliminado === false) {
                               return (
                                 <tr>
                                   <td>
