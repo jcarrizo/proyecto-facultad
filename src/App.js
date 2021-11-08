55645e28f9f5479875b918020eb03979427f0a5b
Jesus Carrizo <jcarrizo15@gmail.com>
2021-11-08 15:34:32 -0300

    guardar rutas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import Rutas from './ruta/Rutas';
+
 import '../src/css/styles.css'
 
 function App() {
   return (
     <div>
-      <Rutas></Rutas>
+
       <ToastContainer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Rutas from './ruta/Rutas';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Rutas></Rutas>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
